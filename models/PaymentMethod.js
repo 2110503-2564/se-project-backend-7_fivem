47a37f8bc20a277844a769d09590279009aa376f
Phoovich <Phoovich@gmail.com>
2025-04-20 14:31:42 +0700

    modified controllers/paymentmethods.js and models/PaymentMethod.js

--- models/PaymentMethod.js
+++ models/PaymentMethod.js
@@ -1,83 +1,78 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const bankNames = [
-    'KBank',        // กสิกรไทย
-    'SCB',          // ไทยพาณิชย์
-    'BBL',          // กรุงเทพ
-    'Krungsri',     // กรุงศรีอยุธยา
-    'KTB',          // กรุงไทย
-    'TTB',          // ทหารไทยธนชาต
-    'BAAC',         // ธ.ก.ส.
-    'GSB',          // ออมสิน
-    'CIMB',
-    'UOB'
+  "KBank", // กสิกรไทย
+  "SCB", // ไทยพาณิชย์
+  "BBL", // กรุงเทพ
+  "Krungsri", // กรุงศรีอยุธยา
+  "KTB", // กรุงไทย
+  "TTB", // ทหารไทยธนชาต
+  "BAAC", // ธ.ก.ส.
+  "GSB", // ออมสิน
+  "CIMB",
+  "UOB",
 ];
 
 const PaymentMethodSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  method: {
+    type: String,
+    enum: ["credit_card", "bank_account"],
+    required: [true, "Please select a payment method"],
+  },
+  cardNumber: {
+    type: String,
+    minlength: 13,
+    maxlength: 19,
+    select: false,
+    required: function () {
+      return this.method === "credit_card";
     },
-    method: {
-        type: String,
-        enum: ['credit_card', 'bank_account'],
-        required: [true, 'Please select a payment method']
+  },
+  // Bank account fields
+  bankAccountNumber: {
+    type: String,
+    select: false,
+    match: [
+      /^\d{10,12}$/, // 10 ถึง 12 ตัวเลขเท่านั้น
+      "Please provide a valid Thai bank account number (10-12 digits)",
+    ],
+    required: function () {
+      return this.method === "bank_account";
     },
-    cardNumber: {
-        type: String,
-        minlength: 13,
-        maxlength: 19,
-        select: false,
-        required: function () {
-            return this.method === 'credit_card';
-        }
+  },
+  bankName: {
+    type: String,
+    enum: bankNames,
+    required: function () {
+      return this.method === "bank_account";
     },
-    // Bank account fields
-    bankAccountNumber: {
-        type: String,
-        select: false,
-        match: [
-            /^\d{10,12}$/, // 10 ถึง 12 ตัวเลขเท่านั้น
-            'Please provide a valid Thai bank account number (10-12 digits)'
-        ],
-        required: function () {
-            return this.method === 'bank_account';
-        }
-    },
-    bankName: {
-        type: String,
-        enum: bankNames,
-        required: function () {
-            return this.method === 'bank_account';
-        }
-    },
-    label: {
-        type: String,
-        maxlength: 50,
-        default: ''
-    },
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Encrypt sensitive fields before saving
-PaymentMethodSchema.pre('save', async function (next) {
-    if (this.method === 'credit_card' && this.isModified('cardNumber')) {
-        const salt = await bcrypt.genSalt(10);
-        this.cardNumber = await bcrypt.hash(this.cardNumber, salt);
-    }
+PaymentMethodSchema.pre("save", async function (next) {
+  if (this.method === "credit_card" && this.isModified("cardNumber")) {
+    const salt = "$2b$10$123456789012345678901u";
+    this.cardNumber = await bcrypt.hash(this.cardNumber, salt);
+  }
 
-    if (this.method === 'bank_account' && this.isModified('bankAccountNumber')) {
-        const salt = await bcrypt.genSalt(10);
-        this.bankAccountNumber = await bcrypt.hash(this.bankAccountNumber, salt);
-    }
+  if (this.method === "bank_account" && this.isModified("bankAccountNumber")) {
+    const salt = "$2b$10$123456789012345678901u";
+    this.bankAccountNumber = await bcrypt.hash(this.bankAccountNumber, salt);
+  }
 
-    //next();
+  //next();
 });
 
 // Compare methods
@@ -90,4 +85,4 @@ PaymentMethodSchema.methods.matchBankAccountNumber = async function (enteredNumb
     return await bcrypt.compare(enteredNumber, this.bankAccountNumber);
 };*/
 
-module.exports = mongoose.model('PaymentMethod', PaymentMethodSchema);
+module.exports = mongoose.model("PaymentMethod", PaymentMethodSchema);
